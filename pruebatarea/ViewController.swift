349c0a4f6eb5ed1f4f252dd9b7eb77f768796c22
Ignacio Hdz <crash_gexihm@hotmail.com>
2016-03-06 23:06:26 -0600

    Se agrega codigo de tarea semana 2

--- pruebatarea/ViewController.swift
+++ pruebatarea/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //modificacion tarea 1
-
+    /* NOTA IMPORTANTE: 
+        El còdigo lo tuve que poner dentro de una aplicacion distinta a la de un playground ya que
+        con el playground no se habilitaban las opciones para hacer push y commit y poderlo subir al repositorio de GitHub
+    */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*: miniReto condicionar numeros del 1 al 100
+        
+        2.Si el numero es divisible entre 5 imprime el numero + "#Bingo!!"
+        3.Si el numero es par, el numero + la palabra "Par"
+        4.Si el numero es impar, el numero + la palabra "Impar"
+        1.Si el numero se encuentra en el rango de 30-40, numero + las palabras "Viva Swift!"
+        */
+        
+        
+        let numero = 1...100
+        var palabra : String = ""
+        let divisor : Int = 5
+        let par : Int = 2
+        let mod : Int = 0
+        var num : String = ""
+        
+        for it in numero{
+            //Inician condiciones de numeros...
+            if it > 29 && it < 41 {
+                palabra = "Viva Swift!!!"
+            }else if it % divisor == mod {
+                palabra = "Bingo!!!"
+            }else if it % par == mod {
+                palabra = "Par!!!"
+            }else {
+                palabra = "Impar!!!"
+            }
+            /* Condicion que imprime los numeros de un digito con un 0 para una vista uniforme
+            y asì practicar la impresion con concatenacion de variables e interpolaciòn */
+            if it > 0 && it < 10 {
+                num = "0" + String(it)
+                print("# " + num + " \t" + palabra)
+            }else {
+                print("# \(it) \t" + palabra)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
